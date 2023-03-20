12188579585cb6084a9b80cab8f8e404380643ca
Edward Ibeh-Young <edward.ibehyoung@gmail.com>
2023-03-19 17:22:53 -0700

    Home-f

--- X_clone/global.js
+++ X_clone/global.js
@@ -5,34 +5,59 @@ const MobileNavList = document.querySelector('.Nav__List__Mobile')
 const openNav = document.querySelector('.Hamburger')
 const closeNav = document.querySelector('.Nav__List__Mobile--icon')
 
-// Fix close nav bug
+if (window.innerWidth <= 1300) collapse();
 const mediaQuery = window.matchMedia('(max-width:1300px)');
-mediaQuery.addEventListener('change', collapse)
+mediaQuery.addEventListener('change', (e) => {
+    if (e.matches) collapse()
+    else expand()
+})
 openNav.addEventListener('click', () => {
-    openNav.style.opacity = 0;
+    console.log('open clicked')
     MobileNavList.style.right = '0px'
 })
 closeNav.addEventListener('click', () => {
-    console.log('ran closed')
-    openNav.style.opacity = 1
+    console.log('close clicked')
     MobileNavList.style.right = '-500px'
 })
 
-function collapse(e) {
-    if (e.matches) {
-        console.log(NavList_1.children[1].children[0].textContent)
-        for (let i = 1; i < NavList_1.children.length; i++){
-            MobileNavList.innerHTML +=
-                `<li class="Nav__List__Mobile--link"><a href="">
-                    ${NavList_1.children[i].children[0].textContent}
-                </a></li>`;
-            NavList_1.children[i].style.display = 'none'
-        }
-    } else {
-        for (let i = 1; i < NavList_1.children.length; i++){
-            MobileNavList.lastChild.remove()
-            NavList_1.children[i].style.display = 'block'
-        }
+
+function collapse() {
+    for (let i = 1; i < NavList_1.children.length; i++){
+        let link = document.createElement('li') 
+        link.className = 'Nav__List__Mobile--link'
+        link.textContent = NavList_1.children[i].children[0].textContent
+
+        MobileNavList.appendChild(link)
+        NavList_1.children[i].style.display = 'none'
     }
+    console.log(MobileNavList.children)
+}
+
+function expand() {
+    for (let i = 1; i < NavList_1.children.length; i++){
+        MobileNavList.lastChild.remove()
+        NavList_1.children[i].style.display = 'block'
+    }
+}
+
+
+let prevScrollpos = window.scrollY;
+
+window.addEventListener('scroll', () => {
+    const currentScrollpos = window.scrollY;
+    if (prevScrollpos > currentScrollpos) handleScrollUp();
+    else if(prevScrollpos < currentScrollpos) handleScrollDown()
+    prevScrollpos = currentScrollpos
+})
+
+function handleScrollUp(){
+    header.style.opacity = 1;
+    header.style.top = '0vh'
+    // header.style.background = 'rgba(0, 0, 0, 0.945)'
+}
+
+function handleScrollDown(){
+    header.style.opacity = 0;
+    header.style.top = '-10vh'
 }
 
